1902182cfaf0719ccb0db8bb8fde3c8095cbf8ca
admin <admin@macbook.local>
2020-04-09 23:56:21 +0300

    Program for Covid-19 in Russia

---
+++ Covid-19/covid_russia.py
@@ -0,0 +1,26 @@
+from covid import Covid
+import time
+
+def print_covid(info, i):
+	print('#', i)
+	print('Russia\nconfirmed:\t', info['confirmed'])
+	print('active:\t\t', info['active'])
+	print('deaths:\t\t', info['deaths'])
+	print('recovered:\t', info['recovered'])
+	print()
+
+covid = Covid()
+
+i = 1
+russia_cases = covid.get_status_by_country_name("russia")
+print_covid(russia_cases, i)
+
+while True:
+	russia_new_cases = covid.get_status_by_country_name("russia")
+	if russia_new_cases['last_update'] > russia_cases['last_update']:
+		print_covid(russia_new_cases, i)
+		russia_cases = russia_new_cases
+		i += 1
+
+	time.sleep(1)
+	pass
