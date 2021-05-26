1f8a33e2cad66fec8339d052569bdecaad49b0a8
Konstantin Ignakov <ignakov37@gmail.com>
2021-05-26 14:54:50 +0300

    Выполнена 5 л.р.

--- Второй курс/Вычислительная математика/lr 5/NewtonSolution.h
+++ Второй курс/Вычислительная математика/lr 5/NewtonSolution.h
@@ -43,14 +43,8 @@ vv atFiniteDifferences(const vv &coordinates) {
     return finiteDifferences;
 }
 
-/// Я тоже не ебу зачем ещё цикл, влад почему-то считает
-/// всё по шагам, сначала первая часть, потом середина
-/// и конец
-
 /// Вторая производная
 vv atFirstDerivative(const vv &coordinates) {
-
-    /// Измените тут значения под ваш массив данных
 
     //Вычисление шага
     double h = coordinates[1][0] - coordinates[0][0];
@@ -81,8 +75,6 @@ vv atFirstDerivative(const vv &coordinates) {
                        / h;
     }
 
-    /// У Влада ещё 2 цикла, у меня числа в целом норм и с одним
-
     //Нахождение производных для следующих 3 членов
     //выделяем координаты необходимых точек
     tempArr = vv(6, v(2));
@@ -108,41 +100,11 @@ vv atFirstDerivative(const vv &coordinates) {
                        / h;
     }
 
-    /// Я хуй знает что это, но если это запустить значения производной у меня будут ебейшие
-
-//    //Нахождение производных для последних 6ти членов
-//    //выделяем координаты необходимых точек
-//    tempArr = vv(6, v(2));
-//    for (int i = 9, j = 0; i < 12; i++, j++) {
-//        tempArr[j][0] = coordinates[i][0];
-//        tempArr[j][1] = coordinates[i][1];
-//    }
-//    //Получаем список конечных приращений
-//    finiteDifference = atFiniteDifferences(tempArr);
-//
-//
-//    //Расчет производных по формуле
-//    for (int i = 9; i < 12; i++) {
-//        double t = (coordinates[i][0] - coordinates[9][0]) / h;
-//
-//        resArr[i][0] = coordinates[i][0];
-//        resArr[i][1] = (finiteDifference.at(0).at(0)
-//                        + ((2.0 * t - 1) * finiteDifference.at(1).at(0) / factorial(2))
-//                        + ((3.0 * t * t - 6.0 * t + 2) * finiteDifference.at(2).at(0) / factorial(3)
-//                           + ((4.0 * t * t * t + 18.0 * t * t + 22.0 * t - 6.0) * finiteDifference.at(3).at(0)) /
-//                             factorial(4))
-//                        + ((5.0 * t * t * t * t - 40.0 * t * t * t + 105.0 * t * t - 100.0 * t + 24.0) *
-//                           finiteDifference.at(4).at(0) / factorial(5)))
-//                       / h;
-//    }
-
     return resArr;
 
 }
 
 vv atSecondDerivative(const vv &coordinates) {
-
-    /// Измените тут значения под ваш массив данных
 
     //Вычисление шага
     double h = coordinates[1][0] - coordinates[0][0];
@@ -170,34 +132,6 @@ vv atSecondDerivative(const vv &coordinates) {
                              factorial(5)))
                        / (h * h);
     }
-
-    /// Я тоже не ебу зачем ещё цикл, влад почему-то считает
-    /// всё по шагам, сначала первая часть, потом середина
-    /// и конец
-
-//    //Нахождение производных для следующих 3 членов
-//    //выделяем координаты необходимых точек
-//    tempArr = vv(6, v(2));
-//    for (int i = 6, j = 0; i < 12; i++, j++)
-//    {
-//        tempArr[j][0] = coordinates[i][0];
-//        tempArr[j][1] = coordinates[i][1];
-//    }
-//    //Получаем список конечных приращений
-//    finiteDifference = atFiniteDifferences(tempArr);
-//
-//    //Расчет производных по формуле
-//    for (int i = 6; i < 12; i++)
-//    {
-//        double t = (coordinates[i][0] - coordinates[6][0]) / h;
-//
-//        resArr[i][0] = coordinates[i][0];
-//        resArr[i][1] = (finiteDifference.at(1).at(0)
-//                        + ((6.0 * t - 6.0) * finiteDifference.at(2).at(0) / factorial(3))
-//                        + ((12.0*t*t - 36.0*t + 22.0) * finiteDifference.at(3).at(0) / factorial(4)
-//                           + ((20.0*t*t*t - 120.0*t*t + 210.0*t - 100.0) * finiteDifference.at(4).at(0)) / factorial(5)))
-//                       / (h*h);
-//    }
 
     return resArr;
 }
