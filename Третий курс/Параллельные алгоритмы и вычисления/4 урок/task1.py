fdda7deaa0fcff114f5b2206fbd856c55227b19a
Konstantin Ignakov <ignakov37@gmail.com>
2021-11-07 15:16:01 +0300

    End first task.

--- Третий курс/Параллельные алгоритмы и вычисления/4 урок/task1.py
+++ Третий курс/Параллельные алгоритмы и вычисления/4 урок/task1.py
@@ -1,23 +1,23 @@
-from multiprocessing import cpu_count
 import threading
-import math
+from math import factorial
+from multiprocessing import cpu_count
+
 
 CPU = cpu_count()
-
 res = 0
 
+
 def function(x: float, n: int) -> float:
-    return float(x) ** n / math.fact(n)
+    return float(x) ** n / factorial(n)
 
 
 class Calc:
     def __init__(self, func, x: float, n_start: int, n_end: int):
         if n_end < n_start:
-            raise Exeption('End have to be more than start')
+            raise ValueError('End have to be more than start')
         
         self.func = func
         self.x = x
-        self.n = n
         self.n_start = n_start
         self.n_end = n_end
 
@@ -25,32 +25,39 @@ class Calc:
         global res
 
         for n in range(self.n_start, self.n_end):
-            res += self.func(x, n)
-
-    def getRes(self):
-        return self._res
+            res += self.func(self.x, n)
 
 
-x = input('Enter x> ')
-n = 100
+def main():
+    x = float(input('Enter x> '))
+    n = 100
 
-c = Calc(function, x, 0, 100)
+    match input('Command (1 - one thread, 2 - many thread): '):
+        case '1':
+            Calc(function, x, 0, 100)()
 
-# args = []
-# for _ in range(CPU + 1):
-#     args.append(int((n / CPU) * _))
+        case '2':
+            args = []
+            for _ in range(CPU + 1):
+                args.append(int((n / CPU) * _))
 
-# threads = []
-# for i in range(CPU):
-#     t = threading.Thread(target=Calc(function, x, args[i], args[i + 1]), args=())
-#     threads.append(t)
+            threads = []
+            for i in range(CPU):
+                t = threading.Thread(target=Calc(function, x, args[i], args[i + 1]), args=())
+                threads.append(t)
 
-# for thread in threads:
-#     thread.start()  # Запустить потоки на исполнение
+            for thread in threads:
+                thread.start()  # Запустить потоки на исполнение
 
-# for thread in threads:
-#     thread.join()   # Ожидать завершения работы дочерних потоков
+            for thread in threads:
+                thread.join()   # Ожидать завершения работы дочерних потоков
+
+        case _:
+            print('Bad input.')
+
+    print(f'Result: {res}\n',
+          f'Main thread exiting.')
 
 
-print(f'Result: {res}\n',
-      f'Main thread exiting.')
+if __name__ == '__main__':
+    main()
