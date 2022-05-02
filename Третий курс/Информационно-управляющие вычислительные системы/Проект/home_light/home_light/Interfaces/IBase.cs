c6de6857c612cae79459b7f9d4e49533dec4cb9a
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-02 18:32:16 +0300

    add json ignore

--- Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Interfaces/IBase.cs
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Interfaces/IBase.cs
@@ -1,4 +1,6 @@
-﻿namespace home_light.Interfaces
+﻿using System.Text.Json.Serialization;
+
+namespace home_light.Interfaces
 {
     /// <summary>
     /// Base interfaces
@@ -8,14 +10,17 @@
         /// <summary>
         /// Date add entry
         /// </summary>
+        [JsonIgnore]
         public DateTime DateAdd { get; set; }
         /// <summary>
         /// Date update entry
         /// </summary>
+        [JsonIgnore]
         public DateTime DateUpdate { get; set; }
         /// <summary>
         /// Entry is deleted
         /// </summary>
+        [JsonIgnore]
         public bool IsDeleted { get; set; }
     }
 }
