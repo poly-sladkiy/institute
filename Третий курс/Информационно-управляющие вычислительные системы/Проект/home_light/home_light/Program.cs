c83bd794582af360e17195cc1df4baf4fa374ec3
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-02 10:41:10 +0300

    update cors, disable https redirection

--- Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Program.cs
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Program.cs
@@ -1,25 +1,34 @@
 var builder = WebApplication.CreateBuilder(args);
-
+#region builder
 // Add services to the container.
+
+builder.Services.AddCors(options => options.AddPolicy("CorsPolicy",
+                            builder =>
+                            {
+                                builder.AllowAnyHeader()
+                                       .AllowAnyMethod()
+                                       .SetIsOriginAllowed((host) => true)
+                                       .AllowCredentials();
+                            }));
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
+#endregion
 var app = builder.Build();
-
+#region app
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+app.UseCors("CorsPolicy");
 
-app.UseHttpsRedirection();
+
+//app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+#endregion
