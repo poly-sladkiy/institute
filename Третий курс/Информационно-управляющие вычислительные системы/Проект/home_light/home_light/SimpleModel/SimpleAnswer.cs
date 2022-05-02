0989ba1a24a4502d04ad62febd0c4e11b05eeeaf
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-02 18:44:42 +0300

    add simple model

---
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/SimpleModel/SimpleAnswer.cs
@@ -0,0 +1,34 @@
+﻿namespace home_light.SimpleModel
+{
+    /// <summary>
+    /// Simple answer
+    /// </summary>
+    public class SimpleAnswer
+    {
+        /// <summary>
+        /// Base contructor
+        /// </summary>
+        public SimpleAnswer()
+        {
+
+        }
+        /// <summary>
+        /// SimpleAnswer
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="error"></param>
+        public SimpleAnswer(bool state, string error)
+        {
+            this.State = state;
+            this.Error = error;
+        }
+        /// <summary>
+        /// State
+        /// </summary>
+        public bool State { get; set; } = false;
+        /// <summary>
+        /// Error text
+        /// </summary>
+        public string Error { get; set; } = string.Empty;
+    }
+}
