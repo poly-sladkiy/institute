b4702e4d979af9d1ad5698e77a9292358f91c946
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-02 11:00:59 +0300

    create litle controller

---
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Controllers/RoomController.cs
@@ -0,0 +1,23 @@
+﻿using home_light.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace home_light.Controllers
+{
+    /// <summary>
+    /// Controller for the room
+    /// </summary>
+    [ApiController]
+    [Route("api/room/")]
+    public class RoomController : ControllerBase
+    {
+        /// <summary>
+        /// Get all rooms
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet]
+        public ActionResult<List<Room>> Get()
+        {
+            return Ok();
+        }
+    }
+}
