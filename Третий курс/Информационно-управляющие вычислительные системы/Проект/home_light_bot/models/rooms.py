b1e9d5b8d15237ed22059d7707634cbb94854785
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-11 17:59:09 +0300

    create room class

---
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light_bot/models/rooms.py
@@ -0,0 +1,11 @@
+class Rooms:
+    def __init__(self, data: dict = None):
+        self.id = data.get('id')
+        self.name = data.get('name')
+        self.sensors = []  # todo: convert to sensors types
+
+    def __str__(self):
+        return f"Комната: {self.name}"
+
+    def get_sensors(self):
+        return self.sensors
