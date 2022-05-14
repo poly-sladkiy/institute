ba0d896a1bc66f64764b7e05ac310055966c5697
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-14 17:29:35 +0300

    update models

---
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light/home_light/Migrations/20220514142908_UpdateModelsNullDefaultConstraint.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace home_light.Migrations
+{
+    public partial class UpdateModelsNullDefaultConstraint : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
