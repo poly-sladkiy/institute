544c12adba12097f049f65cb7cfd9edb890c9654
Ignakov Konstantin <ignakov37@gmail.com>
2022-05-24 13:29:52 +0300

    Create sensor model

--- Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light_bot/models/sensor.py
+++ Третий курс/Информационно-управляющие вычислительные системы/Проект/home_light_bot/models/sensor.py
@@ -1,21 +1,3 @@
-<<<<<<< HEAD
-from models.room import Room
-
-
-class Sensor:
-    def __init__(self, data: dict = None):
-        self.id = data.get('id')
-        self.name = data.get('name')
-        self.data = []  # todo: convert to data types
-        self.flashlight = []  # todo: convert to data types
-        self.room = Room()
-
-    def __str__(self):
-        return f"Сенсор: {self.name}\nЛампочек: {len(self.flashlight)} шт."
-
-    def get_flashlight(self):
-        return self.flashlight
-=======
 import aiohttp
 from aiogram.utils.callback_data import CallbackData
 
@@ -83,4 +65,3 @@ class Sensor:
         await session.close()
 
         return data
->>>>>>> origin/master
