0c09adb1260a3ef07a1d6cc55cfa8302407c2bc2
Ignakov Konstantin <ignakov37@gmail.com>
2023-03-01 20:11:19 +0300

    create second lab

---
+++ Четвертый курс/ОТИВС/2 лр/ЛР2_Client_Server/ЛР2_Client_Server/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace ЛР2_Client_Server
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.textBox1 = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.textBox2 = new System.Windows.Forms.TextBox();
+			this.button3 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.textBox3 = new System.Windows.Forms.TextBox();
+			this.label2 = new System.Windows.Forms.Label();
+			this.label3 = new System.Windows.Forms.Label();
+			this.textBox4 = new System.Windows.Forms.TextBox();
+			this.button4 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this.textBox1.Location = new System.Drawing.Point(249, 76);
+			this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox1.Name = "textBox1";
+			this.textBox1.Size = new System.Drawing.Size(673, 22);
+			this.textBox1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(365, 159);
+			this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(163, 112);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "Да";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(523, 159);
+			this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(179, 112);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "Нет";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// textBox2
+			// 
+			this.textBox2.Location = new System.Drawing.Point(500, 126);
+			this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox2.Name = "textBox2";
+			this.textBox2.Size = new System.Drawing.Size(52, 22);
+			this.textBox2.TabIndex = 3;
+			this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+			// 
+			// button3
+			// 
+			this.button3.Location = new System.Drawing.Point(365, 278);
+			this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button3.Name = "button3";
+			this.button3.Size = new System.Drawing.Size(336, 54);
+			this.button3.TabIndex = 4;
+			this.button3.Text = "по правилу пределить следующее событие";
+			this.button3.UseVisualStyleBackColor = true;
+			this.button3.Click += new System.EventHandler(this.button3_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(308, 129);
+			this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(30, 16);
+			this.label1.TabIndex = 5;
+			this.label1.Text = "IF S";
+			// 
+			// textBox3
+			// 
+			this.textBox3.Location = new System.Drawing.Point(352, 127);
+			this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox3.Name = "textBox3";
+			this.textBox3.Size = new System.Drawing.Size(49, 22);
+			this.textBox3.TabIndex = 6;
+			// 
+			// label2
+			// 
+			this.label2.AutoSize = true;
+			this.label2.Location = new System.Drawing.Point(437, 129);
+			this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label2.Name = "label2";
+			this.label2.Size = new System.Drawing.Size(49, 16);
+			this.label2.TabIndex = 7;
+			this.label2.Text = "AND R";
+			// 
+			// label3
+			// 
+			this.label3.AutoSize = true;
+			this.label3.Location = new System.Drawing.Point(583, 126);
+			this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+			this.label3.Name = "label3";
+			this.label3.Size = new System.Drawing.Size(57, 16);
+			this.label3.TabIndex = 8;
+			this.label3.Text = "THEN S";
+			// 
+			// textBox4
+			// 
+			this.textBox4.Location = new System.Drawing.Point(680, 122);
+			this.textBox4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.textBox4.Name = "textBox4";
+			this.textBox4.Size = new System.Drawing.Size(71, 22);
+			this.textBox4.TabIndex = 9;
+			// 
+			// button4
+			// 
+			this.button4.Location = new System.Drawing.Point(372, 352);
+			this.button4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.button4.Name = "button4";
+			this.button4.Size = new System.Drawing.Size(328, 38);
+			this.button4.TabIndex = 10;
+			this.button4.Text = "Выполнить событие";
+			this.button4.UseVisualStyleBackColor = true;
+			this.button4.Click += new System.EventHandler(this.button4_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(1051, 516);
+			this.Controls.Add(this.button4);
+			this.Controls.Add(this.textBox4);
+			this.Controls.Add(this.label3);
+			this.Controls.Add(this.label2);
+			this.Controls.Add(this.textBox3);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.button3);
+			this.Controls.Add(this.textBox2);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textBox1);
+			this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ Четвертый курс/ОТИВС/2 лр/ЛР2_Client_Server/ЛР2_Client_Server/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+
+namespace ЛР2_Client_Server
+{
+   
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int[] STOP_;//
+        string[] S_;
+        int[,] rools_;
+        
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // массив событий порождающего сценария множества S
+            S_ = new string[100];// параметр: настроеное на 100 проодукционных праввил с 3 исходами
+            S_[1] = "Data Science ?";
+            S_[2] = "Gamedev ?";
+            S_[3] = "Big company?";
+            S_[4] = "Web?";
+            S_[5] = "Fullstack?";
+            S_[6] = "Back-end?";
+            S_[7] = "ASP.Net Core";
+            S_[8] = "Angular";
+            S_[9] = "R";
+            S_[10] = "Unreal Engine";
+            S_[11] = "C# Unity";
+            S_[12] = "Python Django";
+            S_[13] = "Native?";
+            S_[14] = "Android?";
+            S_[15] = "Flutter";
+            S_[16] = "Kotlin";
+            S_[17] = "Swift";
+            // закодированные логические правила: продукционная БЗ начало
+            //int[,] rools_ = new int[100, 2];// параметр: настроеное на 100 проодукционных праввил с 3 исходами
+            rools_ = new int[100, 2];// параметр: настроеное на 100 проодукционных праввил с 3 исходами
+            //
+            rools_[1, 1] = 9;// кодируем правило if S1 and R1 (да) then S9
+            rools_[1, 0] = 2;// кодируем правило if S1 and R0 (нет) then S2
+            rools_[2, 1] = 3;// кодируем правило if S1 and R1 then S3
+            rools_[2, 0] = 4;// кодируем правило if S1 and R0 then S4 *
+            rools_[3, 1] = 10;// кодируем правило if S1 and R1 then S6
+            rools_[3, 0] = 11;// кодируем правило if S1 and R0 then S5 *        
+            rools_[4, 1] = 5;// кодируем правило if S1 and R1 then S7-
+            rools_[4, 0] = 13;// кодируем правило if S1 and R0 then S8 *
+            rools_[5, 1] = 12;// кодируем правило if S1 and R1 then S10
+            rools_[5, 0] = 6;// кодируем правило if S1 and R0 then S11*
+            rools_[6, 1] = 7;// кодируем правило if S1 and R1 then S12*
+            rools_[6, 0] = 8;// кодируем правило if S1 and R0 then S13*
+			rools_[13, 1] = 14;// кодируем правило if S1 and R1 then S12*
+            rools_[13, 0] = 15;// кодируем правило if S1 and R0 then S13*
+            rools_[14, 1] = 16;// кодируем правило if S1 and R0 then S13*
+            rools_[14, 0] = 17;// кодируем правило if S1 and R0 then S13*
+
+
+
+            // закодированные логические правила: продукционная БЗ окончание
+            //подготавливаем хранилище индексов концевых событий начало
+            STOP_ = new int[100];// параметр: настроеное на 100 проодукционных праввил с 3 исходами
+
+            // при отрацательном значении массива STOP_ событие концевое
+            // метим концевые собятия
+            for (int i = 0; i < 100; i++)
+            {
+                STOP_[i] = 7;
+            }
+
+            STOP_[9] = -7;
+            STOP_[11] = -7;
+            STOP_[10] = -7;
+            STOP_[15] = -7;
+            STOP_[17] = -7;
+            STOP_[16] = -7;
+            STOP_[8] = -7;
+            STOP_[7] = -7;
+            STOP_[12] = -7;
+            
+            // метим концевые собятия
+            //подготавливаем хранилище индексов концевых событий окончание
+            // инициализация стартового события
+            
+            textBox1.Text = S_[1];
+            textBox3.Text = "1";
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "1";
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "0";
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+            //База фактов из 2-х переменных начало
+            int SDB = 0;// антецедент импликации Si - левая часть правила
+            int RDB = 0;// антецедент импликации Rj  - левая часть правила
+            int S_cos_ = 0;// косеквент импликации
+            //наполнение БФ информацией о состоянии предметной области начало
+            SDB = Int32.Parse(textBox3.Text);
+            RDB = Int32.Parse(textBox2.Text);
+            //наполнение БФ информацией о состоянии предметной области окончание
+            //База фактов из 2-х переменных окончание
+                       
+
+            
+
+            
+
+            // процедуры событий  !! события отделены от правил и процедур вывода!
+            // интерпретатор начало // в явном виде нет цикла
+            S_cos_ = rools_[SDB, RDB];
+            // textBox2.Text = S_cos_.ToString();
+            textBox4.Text = Convert.ToString(S_cos_);
+
+            // интерпретатор окончение
+            // button4_Click(sender, e);
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            //string[] S_ = new string[100];// параметр: настроеное на 100 проодукционных праввил с 3 исходами
+            
+               
+
+                        //********************************************************* код кнопки
+          
+
+            int S_cos_ = 0;// косеквент импликации
+            //наполнение БФ информацией о состоянии предметной области начало
+            S_cos_ = Int32.Parse(textBox4.Text);
+            textBox1.Text = S_[S_cos_];
+            textBox3.Text = Convert.ToString(S_cos_);
+
+
+                if (STOP_[S_cos_] < 0)
+                {
+            MessageBox.Show("концевое событие");
+            Environment.Exit(0);
+             }
+            
+            
+            
+            //
+            // дописать процедуру определения концевого события и окончания работы
+           
+            
+        }
+
+       
+    }
+}
